f0d3fc155cb2fa01affc06de5638149bf3f395fa
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-10-26 00:11:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6786754832:AAHAn4vcR5JLDRIfWc_0gApmodewj6MDcJ0")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "29486311"))
